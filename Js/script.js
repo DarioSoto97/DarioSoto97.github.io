64dc1f3096be912158b788ad408c61ff303a56c8
Dario Soto <DarioSoto_97@hotmail.com>
2022-03-25 13:37:00 +0100

    V.4 Responsive

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+        document.getElementById("move-content").classList.toggle('clic-container-all');
+        document.getElementById("salida-menu").classList.toggle('salida-menu');
+
+}
+
